016b9d93789be7ac55f9a5aadc25786ca16fa58f
AbdoulDiallo <ta2014@yopmail.com>
2013-11-24 19:45:42 +0100

    second commit

---
+++ sash-3.7/CL-TP1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "include/AST.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    AST *a1 = new AST(5);
+    AST *a2 = new AST(10.0);
+    AST *b1 = new AST(6);
+    AST *b2 = new AST(11.0);
+    AST *c1 = new AST(7);
+    AST *c2 = new AST(12.0);
+
+    AST *a= new AST(PLUS,a1 , a2);
+    AST *b= new AST(PLUS,b1 , b2);
+    AST *c= new AST(PLUS,a , b);
+
+    string expre = "5+5+7";
+    AST * test = a1->Parse(expre);
+
+
+    /*cout << a->GetvalueDouble()<< endl;
+    cout << a->GetvalueInt()<< endl;*/
+
+    double res =  test->eval();
+
+    cout << res<< endl;
+
+    return 0;
+
+}
